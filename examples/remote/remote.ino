a8fc9bfcbdbc5f8c308c43436059f50e4b04de29
BrittonPlewes <brittonp@gmail.com>
2020-09-27 22:38:36 -0400

    clearer comments

--- examples/remote/remote.ino
+++ examples/remote/remote.ino
@@ -1,6 +1,6 @@
 /*
 
-Simple flash and run through the brightness values on the light
+Simple flash and fade through the brightness values on the light
 
 */
 
@@ -8,6 +8,7 @@ Simple flash and run through the brightness values on the light
 
 #include "GodoxRemote.h"
 
+// initiates remote with transmitter DIN attached to arduino pin 4
 GodoxRemote remote(4);
 
 
@@ -19,12 +20,20 @@ void setup(){
     remote.setChannel(0); //1
     remote.setGroup(0);   //A
 
-    remote.setRepeatDelay(25); //remote uses 35-45
-    remote.setRepeatNum(2);    //remote uses 4-5
+
+    //number of times to repeat signal when sent
+    //remote repeats 4-5 times per button press
+    remote.setRepeatNum(2);    
+
+    // delay between repetitions in ms
+    // remote uses 35-45ms, but 10-25 can be used reliably for faster response
+    remote.setRepeatDelay(25); 
 }
 
 
 void loop(){
+    Serial.println("fade up!");
+
     // fade light values up
     for( int i=25; i<=100; i+=5 ){
         remote.setBrightness(i);
@@ -32,18 +41,24 @@ void loop(){
     }
 
     // off/on twice
-    delay(500);
+    delay(500);    
     remote.toggle(0);
+    Serial.println("off");
 
     delay(500);
     remote.toggle(1);
+    Serial.println("on");
 
     delay(500);
-    remote.toggle(0);    
+    remote.toggle(0);
+    Serial.println("off");
 
     delay(500);
-    remote.toggle(1);    
+    remote.toggle(1); 
+    Serial.println("on");   
 
+
+    Serial.println("fade down...");
     // fade light values down
     for( int i=100; i>25; i-=5 ){
         remote.setBrightness(i);
